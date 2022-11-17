6628d94a71910185c7761e1e80eac4a9b6e43b99
ansupaav <anna-sofia.s.paavonen@student.jyu.fi>
2022-11-17 15:12:30 +0200

    testi

---
+++ src/nimigeneraattori/Generaattori.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package nimigeneraattori;
+
+/**
+ * @author Ansku ja Nea
+ *
+ */
+public class Generaattori {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
